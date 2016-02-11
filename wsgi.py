bbf688f7cec5ef156c0dd3edfaafd35ccf985592
James <james.waters@talktalk.net>
2016-02-11 19:43:13 +0000

    tried socket with random port

--- wsgi.py
+++ wsgi.py
@@ -14,7 +14,7 @@ def application(environ, start_response):
     host = environ["OPENSHIFT_PYTHON_IP"]
     port = environ["OPENSHIFT_PYTHON_PORT"]
     print((host, port))
-    serversocket.bind((host, int(8080)))
+    serversocket.bind((host, int(9384)))
     testvar = "Started server on ", str(host) + ":" + str(port)
     #testvar = (host, port)
     
