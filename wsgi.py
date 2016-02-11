4510a3087080770419a0d6a77993191e6a8a00d3
James <james.waters@talktalk.net>
2016-02-11 18:54:27 +0000

    Commented out server bit

--- wsgi.py
+++ wsgi.py
@@ -233,7 +233,7 @@ pre {
 # Below for testing only
 #
 if __name__ == '__main__':
-    from wsgiref.simple_server import make_server
+    """from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
     # Wait for a single request, serve it and quit.
-    httpd.handle_request()
+    httpd.handle_request()"""
