01129baf38eb5cc153284243cefd94e1333b3548
James <james.waters@talktalk.net>
2016-02-13 14:59:11 +0000

    fixed arguments

--- runServer.py
+++ runServer.py
@@ -6,4 +6,4 @@ def run(e1, e2):
     #t1 = threading.Thread(target=flaskServer.startServer)
     #t1.start()
     #socketServer.startServer()
-    flaskServer.startServer(e1, e2own=True)
+    flaskServer.startServer(e1, e2, own=True)
