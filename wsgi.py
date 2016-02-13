60fbe70285803b1e200facd81a84326a92fde4da
James <james.waters@talktalk.net>
2016-02-13 15:08:10 +0000

    added socket stuff after app

--- wsgi.py
+++ wsgi.py
@@ -2,9 +2,13 @@
 # IMPORTANT: Put any additional includes below this line.  If placed above this
 # line, it's possible required libraries won't be in your searchable path
 #
-
+import socketServer
 from flaskServer import app as application
 print("AFTER APP RUN")
+try:
+    socketServer.startServer()
+except Exception as e:
+    print("SOCKET ERROR: ", e)
 
 #
 # Below for testing only
