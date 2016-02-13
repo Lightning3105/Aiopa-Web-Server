3065f7fe1474bd0b4e7549f1da85031266d07a85
James <james.waters@talktalk.net>
2016-02-13 14:52:20 +0000

    added printing of args and kwargs

--- runServer.py
+++ runServer.py
@@ -3,6 +3,8 @@ import socketServer
 import flaskServer
 
 def run(*args, **kwargs):
+    print("ARGS", args)
+    print("KWARGS", kwargs)
     #t1 = threading.Thread(target=flaskServer.startServer)
     #t1.start()
     #socketServer.startServer()
