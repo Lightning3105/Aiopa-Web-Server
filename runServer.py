0a8e945d5a15d1e93e6eecd27cc5b61cf8a5b094
James <james.waters@talktalk.net>
2016-02-24 21:12:23 +0000

    Re added runServer, turns out it was needed. Removed unneeded stuff from it, though

---
+++ runServer.py
@@ -0,0 +1,13 @@
+import flaskServer
+
+def run(e1, e2):
+    #t1 = threading.Thread(target=flaskServer.startServer)
+    #t1.start()
+    #socketServer.startServer()
+    #flaskServer.startServer(e1, e2, own=True)
+    flsk = flaskServer.app.__call__(e1, e2)
+    print(flsk)
+    return flsk
+
+if __name__ == "__main__":
+    flaskServer.startServer(own=True)
