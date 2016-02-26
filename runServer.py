52ea5502a712831446713132fd180d71c0332d8c
James <james.waters@talktalk.net>
2016-02-26 17:51:12 +0000

    Fixed opening of file

--- runServer.py
+++ runServer.py
@@ -5,7 +5,8 @@ def run(e1, e2):
     #t1.start()
     #socketServer.startServer()
     #flaskServer.startServer(e1, e2, own=True)
-    with open("env.txt", "w") as env:
+    import os
+    with open(os.path.join(os.path.dirname(__file__), "env.txt"), "w") as env:
         env.write(e1)
     flsk = flaskServer.app.__call__(e1, e2)
     print(flsk)
