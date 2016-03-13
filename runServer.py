c1d654380aa9a0a0e7553adae40c4e10a56f3f11
James <james.waters@talktalk.net>
2016-03-13 18:22:21 +0000

    Added showing error messages instead of 502 errors

--- runServer.py
+++ runServer.py
@@ -8,6 +8,7 @@ def run(e1, e2):
     import os
     with open(os.path.join(os.path.dirname(__file__), "env.txt"), "w") as env:
         env.write(e1['HTTP_X_CLIENT_IP'])
+    flaskServer.app.debug = True
     flsk = flaskServer.app.__call__(e1, e2)
     return flsk
 
